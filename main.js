a0acc643a4576675abd3fee57f49e58eda8f9b00
Daniele Santillo <daniele.santillo95@gmail.com>
2020-04-20 15:49:42 +0100

    base completata

---
+++ main.js
@@ -0,0 +1,11 @@
+//chiedere dati utente
+var eta = prompt('Quanti anni hai?');
+var km = prompt('Quanti kilometri devi percorrere?');
+//calcolare prezzo biglietto e stampare a video
+var biglietto = km * 0.21;
+if (eta < 18) {
+    biglietto = biglietto - biglietto / 5;
+}else if (eta > 65) {
+    biglietto = biglietto - (biglietto / 5) * 2;
+}
+console.log(biglietto);
